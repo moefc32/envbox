7ebc83fd69031244ea4dd1e12fac9abc29e09e39
Faizal Chan <moefc32@gmail.com>
2025-02-13 04:17:19 +0700

    update dependencies, edit environtments and pm2 configs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
             env: {
                 HOST: process.env.VITE_IPBIND || '127.0.0.1',
                 PORT: parseInt(process.env.VITE_PORT) || 4000,
+                BODY_SIZE_LIMIT: process.env.VITE_SIZE_LIMIT || '10M',
             },
         },
     ]
