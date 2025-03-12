fd0e4d97fe6d9af1ea0a4154ba631244d029c83e
Faizal Chan. <moefc32@gmail.com>
2025-03-12 07:56:33 +0700

    schema get table name from variable

--- src/lib/server/schema.js
+++ src/lib/server/schema.js
@@ -1,13 +1,14 @@
 import sqlite from './sqlite';
+import { TABLE_AUTH, TABLE_ENV } from './model/tables';
 
 export default function setSchema() {
-    const queries = [`
-        CREATE TABLE IF NOT EXISTS auth (
+    const queries = [
+        `CREATE TABLE IF NOT EXISTS ${TABLE_AUTH} (
             id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(16)))),
             email TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL
         );`,
-        `CREATE TABLE IF NOT EXISTS env (
+        `CREATE TABLE IF NOT EXISTS ${TABLE_ENV} (
             id TEXT PRIMARY KEY NOT NULL,
             title TEXT NOT NULL,
             content TEXT,
