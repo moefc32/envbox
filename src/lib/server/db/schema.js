559fdd4645bfd2791d9f590e352c31d364e24bb5
Faizal Chan. <moefc32@gmail.com>
2026-03-15 23:35:40 +0700

    adjust db schema

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -2,17 +2,14 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 import { sql } from 'drizzle-orm';
 
 export const Users = sqliteTable('Users', {
-	id: text('id')
-		.primaryKey()
-		.default(sql`lower(hex(randomblob(16)))`),
+	id: text('id').primaryKey()
+		.$defaultFn(() => sql`lower(hex(randomblob(16)))`),
 	email: text('email').notNull().unique(),
 	password: text('password').notNull(),
 });
 
 export const Envs = sqliteTable('Envs', {
-	id: text('id')
-		.primaryKey()
-		.notNull(),
+	id: text('id').primaryKey().notNull(),
 	title: text('title').notNull(),
 	content: text('content'),
 	timestamp: integer('timestamp').notNull(),
